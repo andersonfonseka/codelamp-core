971e70b52232679f93319a5af876e5f12f50856b
anderson fonseca <anderson.fonseka@gmail.com>
2025-11-21 13:22:13 -0300

    initial version

--- src/main/java/com/codelamp/template/dominio/campo/CampoDouble.java
+++ src/main/java/com/codelamp/template/dominio/campo/CampoDouble.java
@@ -4,6 +4,7 @@ public class CampoDouble extends Campo {
 
 	public CampoDouble(String nome, String rotulo, int posicao) {
 		super("double", nome.substring(0, 1).toLowerCase()+nome.substring(1), rotulo, posicao);
+		this.setTipoView("number");
 	}
 
 }
--- src/main/java/com/codelamp/template/dominio/campo/CampoInteiro.java
+++ src/main/java/com/codelamp/template/dominio/campo/CampoInteiro.java
@@ -3,7 +3,8 @@ package com.codelamp.template.dominio.campo;
 public class CampoInteiro extends Campo {
 
 	public CampoInteiro(String nome, String rotulo, int posicao) {
-		super("number", nome.substring(0, 1).toLowerCase()+nome.substring(1), rotulo, posicao);
+		super("int", nome.substring(0, 1).toLowerCase()+nome.substring(1), rotulo, posicao);
+		this.setTipoView("number");
 	}
 
 }
